55ffeec1434ef8b94a3b1d42f986c3cd0b92824f
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-01-01 17:44:39 +0200

    old unit test removed.

--- test/package.js
+++ test/package.js
@@ -1,16 +1,10 @@
 'use strict';
 
-var expect = require('chai').expect;
-var fless = require('../');
+const expect = require('chai').expect;
+const fless = require('../');
 
 describe('fosify-less', function() {
   it('exports a function', function() {
     expect(fless).to.be.a('function');
-  });
-
-  it('returns the correct set of extensions', function() {
-    expect(fless.extensions).to.be.instanceof(Array);
-    expect(fless.extensions.length).to.equal(1);
-    expect(fless.extensions[0]).to.equal('css');
   });
 });
